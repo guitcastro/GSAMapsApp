a1144362025135097d53570dd2318744da6d6ea7
= <mariovalney@gmail.com>
2014-11-20 01:31:50 -0300

    A little piece of UI

--- src/main/java/com/mariovalney/gsamaps/AboutActivity.java
+++ src/main/java/com/mariovalney/gsamaps/AboutActivity.java
@@ -1,9 +1,22 @@
 package com.mariovalney.gsamaps;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by neo on 18/11/14.
  */
 public class AboutActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
